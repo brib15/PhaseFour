5d5293edfc340f5d9ef8ae142e7df0f6157dc395
brib15 <brib.1005@gmail.com>
2025-03-16 22:15:55 -0700

    Add project files.

---
+++ Accounts.h
@@ -0,0 +1,28 @@
+#ifndef ACCOUNTS
+#define ACCOUNTS
+
+class Customer {
+
+};
+
+class Account {
+
+};
+
+class CheckingsAccount {
+
+};
+
+class SavingsAccount {
+
+};
+
+class MoneyMarketAccount {
+
+};
+
+class CDAccount {
+
+};
+
+#endif
---
+++ PhaseFour.cpp
@@ -0,0 +1,15 @@
+/* Programmers: 
+* Date: 
+*/
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "Accounts.h"
+using namespace std;
+
+int main() {
+
+
+
+	return 0;
+}
